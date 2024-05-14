52fc57dc158213f6191d84963806ca0a07375408
YoungHedgehog <92035751+ELJarzynski@users.noreply.github.com>
2024-05-14 15:01:08 +0200

    Start

--- Readme.md
+++ Readme.md
@@ -92,8 +92,8 @@ X_train, X_val, y_train, y_val = train_test_split(X_train, y_train, test_size=0.
 ![alt table](https://github.com/ELJarzynski/FinalProject-UM/blob/master/photos/Cena%20Oglądanie.png)
 ![alt table](https://github.com/ELJarzynski/FinalProject-UM/blob/master/photos/Cena%20Rok.png)
 ![alt table](https://github.com/ELJarzynski/FinalProject-UM/blob/master/photos/Cena%20Przebieg.png)
-# Na tym inicjalizacji Modeli wybrałem Regresje, ponieważ problem polega na przewidywaniu ceny, która jest zmienną ciągłą
-## Na pierwszy model wybrałem regresje liniową bo prędzej dużo z nią pracowałem
+# Na inicjalizacji Modeli wybrałem Regresje, ponieważ problem polega na przewidywaniu ceny, która jest zmienną ciągłą
+## Na pierwszy model wybrałem regresje liniową
 ```python
 # Inicjalizacja i dopasowanie modelu regresji liniowej
 lm = LinearRegression(n_jobs=-1, fit_intercept=True)
--- datasetPreparing.py
+++ datasetPreparing.py
@@ -111,38 +111,3 @@ import matplotlib.pyplot as plt
 # Wykres zależności ceny od reszty cech
 df.hist(bins=50, figsize=(20, 15))
 plt.show()
-# # Scatter plot dla ceny vs. Mileage
-# plt.figure(figsize=(12, 8))
-# plt.scatter(df['Mileage'], df['Price'], alpha=0.5)
-# plt.xlabel('Mileage')
-# plt.ylabel('Price')
-# plt.title('Price vs. Mileage')
-# plt.grid(True)
-# plt.show()
-#
-# # Scatter plot dla ceny vs. Rating
-# plt.figure(figsize=(12, 8))
-# plt.scatter(df['Rating'], df['Price'], alpha=0.5)
-# plt.xlabel('Rating')
-# plt.ylabel('Price')
-# plt.title('Price vs. Rating')
-# plt.grid(True)
-# plt.show()
-#
-# # Scatter plot dla ceny vs. Review Count
-# plt.figure(figsize=(12, 8))
-# plt.scatter(df['Review Count'], df['Price'], alpha=0.5)
-# plt.xlabel('Review Count')
-# plt.ylabel('Price')
-# plt.title('Price vs. Review Count')
-# plt.grid(True)
-# plt.show()
-#
-# # Scatter plot dla ceny vs. Year Build
-# plt.figure(figsize=(12, 8))
-# plt.scatter(df['Year Build'], df['Price'], alpha=0.5)
-# plt.xlabel('Year Build')
-# plt.ylabel('Price')
-# plt.title('Price vs. Year Build')
-# plt.grid(True)
-# plt.show()
